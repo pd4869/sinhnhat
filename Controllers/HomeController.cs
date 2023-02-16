f8e5f4a02ea5d3853973929178a4894cf45fe5e8
pd4869 <93902278+pd4869@users.noreply.github.com>
2023-02-17 00:08:01 +0700

    sn

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sinh_nhat_cr.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CMSN()
+        {
+            return View();
+        }
+        public ActionResult Present()
+        {
+            return View();
+        }
+}
+}
